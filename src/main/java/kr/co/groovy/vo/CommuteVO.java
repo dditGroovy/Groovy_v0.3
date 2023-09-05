f1fbcf7651bf110ce0a5cc5a0840c83f35ede21a
ChoiDazzi <choidazzi@gmail.com>
2023-09-05 12:47:38 +0900

    insert Test

--- src/main/java/kr/co/groovy/vo/CommuteVO.java
+++ src/main/java/kr/co/groovy/vo/CommuteVO.java
@@ -4,15 +4,17 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.Date;
+
 
 @Getter
 @Setter
 @ToString
 public class CommuteVO {
-    private String dclzWorkDe;
+    private Date dclzWorkDe;
     private String dclzEmplId;
-    private String dclzAttendTm;
-    private String dclzLvffcTm;
+    private Date dclzAttendTm;
+    private Date dclzLvffcTm;
     private int dclzDailWorkTime;
     private int dclzWikWorkTime;
     private String commonCodeLaborSttus;
